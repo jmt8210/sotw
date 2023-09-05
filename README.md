0f458330f772ba68339d6d608f6a7f1050b3b209
Justin Thoms <jmt8210@gmail.com>
2023-09-04 23:47:11 -0400

    Added inital sotw page with leaderboard and info tables

--- README.md
+++ README.md
@@ -1,34 +1,21 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Squad of the Week
 
-## Getting Started
+## Setup
 
-First, run the development server:
+Create a free api key with collegefootballdata.com
+
+> [!WARNING]
+> PLEASE DO NOT ABUSE! This is a very nice free api. Next.js caches responses so we don't send multiple requests.
+
+Add to your `.env.local`:
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
+CFB_DATA_TOKEN=<your-api-key>
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Install and Run
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+```bash
+npm i
+npm run dev
+```
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,161 @@
-import Image from 'next/image'
+import Image from "next/image";
+import * as _ from "lodash";
 
-export default function Home() {
+type WinsItem = {
+  squad: string;
+  wins: number;
+};
+
+type CFBAPIResponse = {
+  // This has more fields, add as needed
+  home_team: string;
+  away_team: string;
+  home_points: number;
+  away_points: number;
+  week: number;
+};
+
+const squads = [
+  "Tubas",
+  "Trumpets",
+  "KAOS",
+  "Percussion",
+  "Guard",
+  "Piccs",
+  "Clarinets",
+  "Saxes",
+];
+
+// Array containing the squad who won for each week
+// Empty for no-squad-of-the-week weeks (E.g. away games (minus travel game))
+const squad_wins: string[] = ["KAOS"];
+
+async function getWins() {
+  // Could call out to route here (if hooking up to db or something similar)
+  // return await fetch('http://localhost:3000/api/wins').then(res => res.json());
+
+  const wins = _.countBy(squad_wins);
+  const data = squads.map((i) => ({ squad: i, wins: wins[i] ?? 0 }));
+
+  return data;
+}
+
+async function getGames() {
+  const data = await fetch(
+    "https://api.collegefootballdata.com/games?year=2023&seasonType=regular&team=maryland",
+    { headers: { Authorization: `Bearer ${process.env.CFB_DATA_TOKEN}` } },
+  ).then((res) => res.json());
+  return data;
+}
+
+function replaceSpaces(text: string) {
+  return text.replace(" ", "_");
+}
+
+export default async function Home() {
+  const wins: WinsItem[] = await getWins();
+  const games = await getGames();
+
+  wins.sort((a, b) => ("" + a.squad).localeCompare(b.squad));
+  if (!wins) return <div></div>;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col bg-maryland-band bg-center min-h-screen items-center justify-center py-4">
+      <div className="relative bg-slate-200 rounded-xl overflow-auto mb-4">
+        {/* Title */}
+        <h1 className="dark:border-slate-600 font-medium p-4 text-slate-600 dark:text-slate-600 text-left">
+          MSOM Squad of the Week 2023 Wins
+        </h1>
+      </div>
+      <div className="relative bg-slate-200 rounded-xl overflow-auto w-3/4">
+        {/* Wins */}
+        <div className="shadow-sm overflow-hidden overflow-x-scroll my-8">
+          <table className="border-collapse table-auto w-full text-sm">
+            <thead>
+              <tr>
+                <th className="header-cell">Squad</th>
+                <th className="header-cell">Wins</th>
+              </tr>
+            </thead>
+            <tbody className="bg-white dark:bg-slate-800">
+              <>
+                {wins.map((item: WinsItem) => (
+                  <tr key={item.squad}>
+                    <td className="data-cell">{item.squad}</td>
+                    <td className="data-cell">{item.wins}</td>
+                  </tr>
+                ))}
+              </>
+            </tbody>
+          </table>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <>
+        {games !== undefined && (
+          <div className="relative bg-slate-200 rounded-xl overflow-auto w-3/4 mt-4">
+            {/* Games */}
+            <div className="shadow-sm overflow-hidden overflow-x-scroll my-8">
+              <table className="border-collapse table-auto w-full text-sm">
+                <thead>
+                  <tr>
+                    <th className="header-cell">Home</th>
+                    <th className="header-cell">Away</th>
+                    <th className="header-cell">Home Score</th>
+                    <th className="header-cell">Away Score</th>
+                    <th className="header-cell">Squad of the Week</th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white dark:bg-slate-800">
+                  <>
+                    {games.map((game: CFBAPIResponse) => (
+                      <tr key={game.home_team + game.week}>
+                        <td className="data-cell">
+                          <div className="flex flex-row gap-2 items-center">
+                            <Image
+                              src={`./logos/${replaceSpaces(
+                                game.home_team,
+                              )}.svg`}
+                              alt={`${game.home_team} logo`}
+                              width={24}
+                              height={24}
+                            />
+                            {game.home_team}
+                          </div>
+                        </td>
+                        <td className="data-cell">
+                          <div className="flex flex-row gap-2 items-center">
+                            <Image
+                              src={`./logos/${replaceSpaces(
+                                game.away_team,
+                              )}.svg`}
+                              alt={`${game.away_team} logo`}
+                              width={24}
+                              height={24}
+                            />
+                            {game.away_team}
+                          </div>
+                        </td>
+                        <td className="data-cell">
+                          {game.home_points ?? "N/A"}
+                        </td>
+                        <td className="data-cell">
+                          {game.away_points ?? "N/A"}
+                        </td>
+                        <td className="data-cell">
+                          {game.away_team === "Maryland"
+                            ? "-"
+                            : squad_wins[game.week - 1] ?? ""}
+                        </td>
+                      </tr>
+                    ))}
+                  </>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
+      </>
+    </div>
+  );
 }
